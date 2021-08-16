c4c54f4d4fab3a374fd56137334829c65bc25f20
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-16 23:18:32 +0300

    We then initialize the Migrate class and pass in the app instance

--- manage.py
+++ manage.py
@@ -2,3 +2,7 @@ from app import create_app, db
 from flask_script import Manager, Server
 
 app = create_app('production')
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
