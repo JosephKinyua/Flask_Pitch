5c25b4fd082fb2a5912fe5ea6c474a95939d41c2
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-17 08:33:06 +0300

    email configuration

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
 
 class Config:
-    
+    SECRET_KEY = 'secret'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    UPLOADED_PHOTOS_DEST = 'app/static/photo
