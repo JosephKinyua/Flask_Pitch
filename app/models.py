6b8261fb3aaec2267d12f29d46f1ee926a2dc12c
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-17 15:08:36 +0300

    add database columns

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
 from . import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id= db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    profile_pic = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    comments = db.relationship('Comment', backref='users', lazy= 'dynamic')
+    date_join = db.Column(db.DateTime, default = datetime.utcnow())
