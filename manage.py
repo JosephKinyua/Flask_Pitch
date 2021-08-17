022823ab79c9b14978bf5eabc6d27e46ad8a913f
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-17 20:34:23 +0300

    commit of the project

--- manage.py
+++ manage.py
@@ -4,15 +4,23 @@ from flask_migrate import Migrate, MigrateCommand
 from app.models import User
 
 app = create_app('production')
-migrate = Migrate(app,db)
+
+migrate = Migrate(app, db)
 manager = Manager(app)
-manager.add_command('server',Server)
-manager.add_command('db',MigrateCommand)
+manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+  return dict(app = app, db = db, User = User)
+
 if __name__ == '__main__':
-    manager.run()
+  manager.run()
 
 
