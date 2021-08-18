47072c57401955d593b761c41b05feba669e6422
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-18 21:56:06 +0300

    declaring a login form in a blueprint

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,1 +1,10 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, ValidationError, BooleanField
+from wtforms.validators import Required, Email, EqualTo
+from ..models import User
+
+class LoginForm(FlaskForm):
+  email = StringField('Email Address', validators=[Required(), Email()])
+  password = PasswordField('Password', validators=[Required()])
+  remember = BooleanField('Remember me')
+  submit = SubmitField('Sign In')
