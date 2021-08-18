e3710c07fdef0d99da26c170cf057d222bd20ea0
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-18 23:01:16 +0300

    login_manager

--- app/models.py
+++ app/models.py
@@ -48,81 +48,86 @@ class Pitch(db.Model):
         return cls.query.filter_by(category=group).all()
 
 class Comment(db.Model):
-  __tablename__ = 'comments'
+    __tablename__ = 'comments'
 
-  id = db.Column(db.Integer, primary_key = True)
-  comment = db.Column(db.String(2000))
-  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-  pitch_id = db.Column(db.Integer, db.ForeignKey('pitch.id'))
+    id = db.Column(db.Integer, primary_key = True)
+    comment = db.Column(db.String(2000))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitch.id'))
 
-  def save_comment(self):
-    db.session.add(self)
-    db.session.commit()
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
   
-  @classmethod
-  def get_specific_comment(cls, id):
-    return cls.query.filter_by(pitch_id = id).all()
+    @classmethod
+    def get_specific_comment(cls, id):
+        return cls.query.filter_by(pitch_id = id).all()
 
 
 class Likes(db.Model):
-  __tablename__ = 'likes'
+    __tablename__ = 'likes'
 
-  id = db.Column(db.Integer,primary_key=True)
-  likes = db.Column(db.Integer,default=1)
-  pitch_id = db.Column(db.Integer,db.ForeignKey('pitch.id'))
-  user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    id = db.Column(db.Integer,primary_key=True)
+    likes = db.Column(db.Integer,default=1)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitch.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 
-  def save_likes(self):
+    def save_likes(self):
       db.session.add(self)
       db.session.commit()
 
-  @classmethod
-  def add_likes(cls,id):
-      likes_pitch = cls(user = current_user, pitch_id=id)
-      likes_pitch.save_likes()
+    @classmethod
+    def add_likes(cls,id):
+        likes_pitch = cls(user = current_user, pitch_id=id)
+        likes_pitch.save_likes()
 
   
-  @classmethod
-  def get_likes(cls,id):
-      likes = cls.query.filter_by(pitch_id=id).all()
-      return likes
+    @classmethod
+    def get_likes(cls,id):
+        likes = cls.query.filter_by(pitch_id=id).all()
+        return likes
 
-  @classmethod
-  def get_all_likes(cls):
-      likes = cls.query.order_by('id').all()
-      return likes
+    @classmethod
+    def get_all_likes(cls):
+        likes = cls.query.order_by('id').all()
+        return likes
 
-  def __repr__(self):
-      return f'{self.user_id}:{self.pitch_id}'
-      
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
+
 
 class Dislikes(db.Model):
-  __tablename__ = 'dislikes'
+    __tablename__ = 'dislikes'
 
-  id = db.Column(db.Integer,primary_key=True)
-  dislikes = db.Column(db.Integer,default=1)
-  pitch_id = db.Column(db.Integer,db.ForeignKey('pitch.id'))
-  user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    id = db.Column(db.Integer,primary_key=True)
+    dislikes = db.Column(db.Integer,default=1)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitch.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 
-  def save_dislikes(self):
-      db.session.add(self)
-      db.session.commit()
+    def save_dislikes(self):
+        db.session.add(self)
+        db.session.commit()
 
-  @classmethod
-  def add_dislikes(cls,id):
-      dislikes_pitch = cls(user = current_user, pitch_id=id)
-      dislikes_pitch.save_dislikes()
+    @classmethod
+    def add_dislikes(cls,id):
+        dislikes_pitch = cls(user = current_user, pitch_id=id)
+        dislikes_pitch.save_dislikes()
 
   
-  @classmethod
-  def get_dislikes(cls,id):
-      dislikes = cls.query.filter_by(pitch_id=id).all()
-      return dislikes
+    @classmethod
+    def get_dislikes(cls,id):
+        dislikes = cls.query.filter_by(pitch_id=id).all()
+        return dislikes
 
-  @classmethod
-  def get_all_dislikes(cls):
-      dislikes = cls.query.order_by('id').all()
-      return dislikes
+    @classmethod
+    def get_all_dislikes(cls):
+        dislikes = cls.query.order_by('id').all()
+        return dislikes
 
-  def __repr__(self):
-      return f'{self.user_id}:{self.pitch_id}'
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
+        
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
