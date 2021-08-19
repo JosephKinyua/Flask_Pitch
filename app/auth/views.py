234ba3ecebdbfc7aa300e6bef1b0fb72d48d72ed
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-19 09:27:06 +0300

    add base html

--- app/auth/views.py
+++ app/auth/views.py
@@ -11,11 +11,10 @@ def login():
 
     login_form = LoginForm()
     if login_form.validate_on_submit():
-
-        user = User.query.filter_by(email = login_form.email.data).first()
+      user = User.query.filter_by(email = login_form.email.data).first()
     if user is not None and user.verify_password(login_form.password.data):
-        login_user(user, login_form.remember.data)
-        return redirect(request.args.get('next') or url_for('main.profile', uname=current_user.username))
+      login_user(user, login_form.remember.data)
+      return redirect(request.args.get('next') or url_for('main.profile', uname=current_user.username))
     flash('Invalid username or password')
     title = 'Login'
   
